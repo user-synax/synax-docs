d5b08ff57d457f697cec608e650cd6b396325bf6
user-synax <usersynax@gmail.com>
2026-05-22 20:05:13 +0530

    models: add User schema

---
+++ models/User.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+  },
+  avatarUrl: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  deletedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+
+export default User;
